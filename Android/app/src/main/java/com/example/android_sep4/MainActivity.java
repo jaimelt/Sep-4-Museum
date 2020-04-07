fe64510f4ea37c67531e9431de6aff0321048491
Valeriu Rusu <280167@via.dk>
2020-04-07 13:12:12 +0200

    Created the tab menu

--- Android/app/src/main/java/com/example/android_sep4/MainActivity.java
+++ Android/app/src/main/java/com/example/android_sep4/MainActivity.java
@@ -1,14 +1,60 @@
 package com.example.android_sep4;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.tabs.TabItem;
+import com.google.android.material.tabs.TabLayout;
 
+public class MainActivity extends AppCompatActivity {
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+    private TabItem ArtworksTab, MuseumTab, RoomsTab;
+    private PageAdapter pageAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tabLayout = findViewById(R.id.tablayout);
+        ArtworksTab = findViewById(R.id.tab1);
+        MuseumTab = findViewById(R.id.tab2);
+        RoomsTab = findViewById(R.id.tab3);
+        viewPager = findViewById(R.id.viewpager);
+
+        pageAdapter = new PageAdapter(getSupportFragmentManager(), tabLayout.getTabCount());
+        viewPager.setAdapter(pageAdapter);
+
+        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+                if(tab.getPosition() == 0) {
+                    pageAdapter.notifyDataSetChanged();
+                } else if(tab.getPosition() == 1) {
+                    pageAdapter.notifyDataSetChanged();
+                } else if(tab.getPosition() == 2) {
+                    pageAdapter.notifyDataSetChanged();
+                }
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+
+
+
     }
 }
---
+++ Android/app/src/main/java/com/example/android_sep4/RoomsTab.java
@@ -0,0 +1,28 @@
+package com.example.android_sep4;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class RoomsTab extends Fragment {
+
+    public RoomsTab() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_rooms_tab, container, false);
+    }
+}
