8ff39a0347daa73b2d2c279b6094dc3ea3b2a690
Luemantis <ledaodavid@gmail.com>
2020-04-08 18:13:41 +0200

    Fixing gradle & Resources

--- Android/app/src/main/java/com/example/android_sep4/view/NewArtworkActivity.java
+++ Android/app/src/main/java/com/example/android_sep4/view/NewArtworkActivity.java
@@ -3,6 +3,7 @@ package com.example.android_sep4.view;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import com.example.android_sep4.R;
 
 public class NewArtworkActivity extends AppCompatActivity {
 
