8e56a72c829339d70e3cf0c4a31dbe9f3d4f4581
Luemantis <ledaodavid@gmail.com>
2020-04-08 18:02:19 +0200

    New activity

---
+++ Android/app/src/main/java/com/example/android_sep4/view/NewArtworkActivity.java
@@ -0,0 +1,14 @@
+package com.example.android_sep4.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NewArtworkActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_artwork_acitivity);
+    }
+}
