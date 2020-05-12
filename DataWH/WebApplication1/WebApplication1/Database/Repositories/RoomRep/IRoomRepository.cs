8abc2e1f8ed62b03f6254c562366834e14c8598f
xplameboy <35765016+xplameboy@users.noreply.github.com>
2020-05-12 16:01:50 +0200

    Controller and Rep added

---
+++ DataWH/WebApplication1/WebApplication1/Database/Repositories/RoomRep/IRoomRepository.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Datamodel;
+
+namespace WebApplication1.Database.Repositories.RoomRep
+{
+    interface IRoomRepository
+    {
+
+        Task<IEnumerable<Room>> getAllRoomsAsync();
+
+        Task<Room> GetRoomByLocationCodeAsync(string roomLocationCode);
+
+        Task<Room> GetRoomWithDetailsAsync(string roomLocationCode);
+
+        Task<Room> GetRoomOptimalMeasurementsAsync(string roomLocationCode);
+
+        Task<Room> GetRoomMeasurementConditionsAsync(string roomLocationCode);
+
+        void createRoom(Room room);
+
+        void updateRoom(Room room);
+
+        void deleteRoom(Room room);
+
+        bool roomExists(string roomLocationCode);
+
+        void saveChanges();
+
+    }
+}
