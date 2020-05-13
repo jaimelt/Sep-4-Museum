4dbc4bf088c779c685be4471ac28f11d47d18372
Marina Ionel <marinicaionel@gmail.com>
2020-05-13 15:36:26 +0200

    iot project

---
+++ IoT/IoTApp/drivers/docs/navtreeindex2.js
@@ -0,0 +1,12 @@
+var NAVTREEINDEX2 =
+{
+"structtsl2591_combined_data__t.html#a848f746c6806de62a952d1344888af47":[8,9,2,0,2],
+"todo.html":[7],
+"tsl2591_8h.html":[10,0,16],
+"tsl2591_8h_source.html":[10,0,16],
+"tsl2591_driver_quick_start.html":[6],
+"tsl2591_driver_quick_start.html#tsl2591_driver_use_cases":[6,0],
+"tsl2591_driver_quick_start.html#tsl2591_make_measuring":[6,2],
+"tsl2591_driver_quick_start.html#tsl2591_make_measuring2":[6,2,0],
+"tsl2591_driver_quick_start.html#tsl2591_setup_use_case":[6,1]
+};
---
+++ IoT/IoTApp/drivers/docs/search/enums_4.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['tsl2591gain_5ft',['tsl2591Gain_t',['../group__tsl2591__driver__structs.html#ga06d8488f0d5ff6c4b06618a86ebf48b5',1,'tsl2591.h']]],
+  ['tsl2591integrationtime_5ft',['tsl2591IntegrationTime_t',['../group__tsl2591__driver__structs.html#gaa30b28bcfaad3e119a0cf834e0cc56a4',1,'tsl2591.h']]],
+  ['tsl2591returncode_5ft',['tsl2591ReturnCode_t',['../group__tsl2591__driver__return__codes.html#ga6025f4dda22afe8a3c91d6d43287b478',1,'tsl2591.h']]]
+];
