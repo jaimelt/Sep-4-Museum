028be327d6ab20dc824c456ef4c6cf1e11ffafd1
Florin Bordei <florin.brd@gmail.com>
2020-05-13 12:33:58 +0200

    dasd
    asd

--- DataWH/WebApplication1/WebApplication1/Datamodel/Artwork.cs
+++ DataWH/WebApplication1/WebApplication1/Datamodel/Artwork.cs
@@ -12,7 +12,17 @@ namespace WebApplication1.Datamodel
         public string Author { get; set; }
         public string Location { get; set; }
         
-        public ArtworkMeasurement ArtworkMeasurement { get; set; }
+        public int MinLight { get; set; }
+        public int MaxLight { get; set; }
+        
+        public int MinCo2 { get; set; }
+        public int MaxCo2 { get; set; }
+        
+        public int MinTemperature { get; set; }
+        public int MaxTemperature { get; set; }
+        
+        public int MinHumidity { get; set; }
+        public int MaxHumidity { get; set; }
         
     }
 }
