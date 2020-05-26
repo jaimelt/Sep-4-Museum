1e6b9ea85ad95d715969cbdab6728e62c37b1496
Marina Ionel <marinicaionel@gmail.com>
2020-05-26 14:14:18 +0200

    testing base

--- IoT/IoTApp/src/main.c
+++ IoT/IoTApp/src/main.c
@@ -2,22 +2,13 @@
 #include <avr/io.h>
 #include <avr/sfr_defs.h>
 
-#include <hal_defs.h>
-#include <ihal.h>
-
 #include <ATMEGA_FreeRTOS.h>
-#include <semphr.h>
 
 #include <FreeRTOSTraceDriver.h>
 #include <stdio_driver.h>
 #include <serial.h>
-#include <avr/interrupt.h>
-#include <lora_driver.h>
 
 #include "tasks/sensor_control_task.h"
-#include "setup/setup_drivers.h"
-
-SemaphoreHandle_t _xPrintfSemaphore =NULL;
 
 int main(void)
 {
@@ -30,7 +21,5 @@ int main(void)
 
 	vTaskStartScheduler();
 
-	while (1)
-	{
-	}
+	while (1) {}
 }
