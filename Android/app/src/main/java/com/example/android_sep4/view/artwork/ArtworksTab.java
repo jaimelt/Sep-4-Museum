ecc714b21b43f33d809c2e91543adb8c8d6604b9
Luemantis <ledaodavid@gmail.com>
2020-05-28 15:25:01 +0200

    Settings folder

--- Android/app/src/main/java/com/example/android_sep4/view/artwork/ArtworksTab.java
+++ Android/app/src/main/java/com/example/android_sep4/view/artwork/ArtworksTab.java
@@ -25,7 +25,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.example.android_sep4.R;
 import com.example.android_sep4.adapters.ArtworksAdapter;
 import com.example.android_sep4.view.AccountActivity;
-import com.example.android_sep4.view.SettingsActivity;
+import com.example.android_sep4.view.settings.SettingsActivity;
 import com.example.android_sep4.view.VisitorsActivity;
 import com.example.android_sep4.viewmodel.artwork.ArtworksTabViewModel;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
