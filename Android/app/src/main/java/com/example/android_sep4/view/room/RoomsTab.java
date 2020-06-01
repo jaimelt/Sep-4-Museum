8b881bb4f048f8430c05f328c893284d64acd862
Luemantis <ledaodavid@gmail.com>
2020-06-01 15:54:26 +0200

    Update RoomsTab.java

--- Android/app/src/main/java/com/example/android_sep4/view/room/RoomsTab.java
+++ Android/app/src/main/java/com/example/android_sep4/view/room/RoomsTab.java
@@ -14,6 +14,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Adapter;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -22,6 +23,7 @@ import androidx.core.app.NotificationManagerCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.preference.PreferenceManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -42,6 +44,7 @@ public class RoomsTab extends Fragment implements SharedPreferences.OnSharedPref
     private RoomsAdapter adapter;
     private ProgressBar progressBar;
     private RecyclerView recyclerView;
+    private SharedPreferences sharedPreferences;
 
     public RoomsTab() {
         // Required empty public constructor
@@ -52,9 +55,20 @@ public class RoomsTab extends Fragment implements SharedPreferences.OnSharedPref
                              Bundle savedInstanceState) {
         setViewModel();
         setHasOptionsMenu(true);
+        setSharePreferenceChangeListener();
         return inflater.inflate(R.layout.fragment_rooms_tab, container, false);
     }
 
+    private void setSharePreferenceChangeListener() {
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getContext());
+        sharedPreferences.registerOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        sharedPreferences.unregisterOnSharedPreferenceChangeListener(this);
+    }
 
     public void setViewModel() {
         roomsTabViewModel = new ViewModelProvider(this).get(RoomsTabViewModel.class);
@@ -115,8 +129,11 @@ public class RoomsTab extends Fragment implements SharedPreferences.OnSharedPref
 
     @Override
     public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        Toast.makeText(getContext(), "IT CHANGED", Toast.LENGTH_SHORT).show();
         if (key.equals(getString(R.string.pref_temperature_key))) {
-            adapter.notifyDataSetChanged();
+            roomsTabViewModel.getRooms().observe(getViewLifecycleOwner(), rooms -> {
+                adapter.setRooms(rooms);
+            });
         }
     }
 }
