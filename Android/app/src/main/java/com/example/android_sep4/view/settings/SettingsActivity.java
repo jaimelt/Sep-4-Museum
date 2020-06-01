b000ecf8ed4886e6c5defe499a9ac2e3774efef3
Luemantis <ledaodavid@gmail.com>
2020-06-01 10:25:18 +0200

    Settings

--- Android/app/src/main/java/com/example/android_sep4/view/settings/SettingsActivity.java
+++ Android/app/src/main/java/com/example/android_sep4/view/settings/SettingsActivity.java
@@ -1,6 +1,7 @@
 package com.example.android_sep4.view.settings;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
@@ -12,5 +13,18 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        Toolbar toolbar = findViewById(R.id.settings_toolbar);
+        setSupportActionBar(toolbar);
+        setTitle("Settings");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    //finish on activity when up navigation is clicked - animation slide to right
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        finish();
+        return true;
     }
 }
