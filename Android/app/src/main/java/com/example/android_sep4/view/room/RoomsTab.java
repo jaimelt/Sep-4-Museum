7f24eb5bb2f1a37a039c3dfe793bf98efd7a0930
Valeriu Rusu <280167@via.dk>
2020-06-01 15:44:50 +0200

    Delete Visitors

--- Android/app/src/main/java/com/example/android_sep4/view/room/RoomsTab.java
+++ Android/app/src/main/java/com/example/android_sep4/view/room/RoomsTab.java
@@ -1,10 +1,7 @@
 package com.example.android_sep4.view.room;
 
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -12,13 +9,10 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Adapter;
 import android.widget.ProgressBar;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.core.app.NotificationCompat;
-import androidx.core.app.NotificationManagerCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
@@ -29,7 +23,6 @@ import com.example.android_sep4.R;
 import com.example.android_sep4.adapters.RoomsAdapter;
 import com.example.android_sep4.view.AccountActivity;
 import com.example.android_sep4.view.settings.SettingsActivity;
-import com.example.android_sep4.view.VisitorsActivity;
 import com.example.android_sep4.viewmodel.roomList.RoomsTabViewModel;
 
 
@@ -94,7 +87,6 @@ public class RoomsTab extends Fragment implements SharedPreferences.OnSharedPref
         MenuItem searchItem = menu.findItem(R.id.search);
         MenuItem manageItem = menu.findItem(R.id.manageAccounts);
         MenuItem settingsItem = menu.findItem(R.id.settings);
-        MenuItem visitorsItem = menu.findItem(R.id.visitors);
         searchItem.setVisible(false);
         manageItem.setOnMenuItemClickListener(item -> {
             startActivity(new Intent(getContext(), AccountActivity.class));
@@ -103,11 +95,6 @@ public class RoomsTab extends Fragment implements SharedPreferences.OnSharedPref
 
         settingsItem.setOnMenuItemClickListener(item -> {
             startActivity(new Intent(getContext(), SettingsActivity.class));
-            return true;
-        });
-
-        visitorsItem.setOnMenuItemClickListener(item -> {
-            startActivity(new Intent(getContext(), VisitorsActivity.class));
             return true;
         });
 
