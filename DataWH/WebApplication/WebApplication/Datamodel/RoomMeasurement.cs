ceb4057a75806e0ffd2cdfb26a38b8beb83fea3e
jaimelt <jaime89jlt@gmail.com>
2020-06-03 10:23:41 +0200

    Revert "mergeFlorin"
    This reverts commit 96e37ec0c035540ff574ee1894ecd70368b6b67c, reversing
    changes made to 26517983e4c249fcb82abe2c5135db049b850e59.

--- DataWH/WebApplication/WebApplication/Datamodel/RoomMeasurement.cs
+++ DataWH/WebApplication/WebApplication/Datamodel/RoomMeasurement.cs
@@ -1,14 +1,11 @@
 using System;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using MongoDB.Bson;
-using MongoDB.Bson.Serialization.Attributes;
 
 namespace WebApplication.Datamodel
 {
     public class RoomMeasurement
     {
-<<<<<<< HEAD
         public RoomMeasurement(int id, decimal light, decimal temperature, decimal humidity, decimal co2, string roomNo)
         {
             Id = id;
@@ -28,33 +25,10 @@ namespace WebApplication.Datamodel
         [Column(TypeName = "decimal(8,4)")] public decimal Temperature { get; set; }
         [Column(TypeName = "decimal(8,4)")] public decimal Humidity { get; set; }
         [Column(TypeName = "decimal(8,4)")] public decimal Co2 { get; set; }
-=======
-        [BsonRepresentation(BsonType.ObjectId)]
-
-        public string MongoId;
         
-        [Key] 
-        public int Id { get; set; }
-        
-        [Column(TypeName = "decimal(8,4)")]
-        public decimal Light { get; set; }
-        [Column(TypeName = "decimal(8,4)")]
-  
-        public decimal Temperature { get; set; }
-        [Column(TypeName = "decimal(8,4)")] 
-     
-        public decimal Humidity { get; set; }
-        [Column(TypeName = "decimal(8,4)")] 
-       
-        public decimal Co2 { get; set; }
->>>>>>> dh.florinx
-        
-       
         public DateTime MeasurementDate { get; set; }
-        
-        public string roomNo { get; set; }
 
-     
+        public string roomNo { get; set; }
 
 
         public void setroomNoFromInt(int roomNo)
