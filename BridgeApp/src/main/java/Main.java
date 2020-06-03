6f64a2249889d870d34ec27573ed198d6df44933
Marina Ionel <marinicaionel@gmail.com>
2020-06-03 15:25:09 +0200

    Update Main.java

--- BridgeApp/src/main/java/Main.java
+++ BridgeApp/src/main/java/Main.java
@@ -1,6 +1,19 @@
+/**
+ * A main class to start the application
+ *
+ * @author Marina Ionel
+ * @version 1.0.0
+ */
 public class Main {
+    /**
+     * The main method for starting the program
+     *
+     * @param args supplied command-line arguments
+     */
     public static void main(String[] args) {
+        //initialize a lorawan client
         new LoRaClient();
+        //an infinite loop to keep the application running
         for (; ; ) ;
     }
 }
