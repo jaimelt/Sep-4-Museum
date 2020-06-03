40618fd93a9e0ba031145f6ef10a9b859537c991
Florin Bordei <florin.brd@gmail.com>
2020-06-03 10:26:43 +0200

    gfd

--- DataWH/WebApplication/WebApplication/Controllers/ArtworkController.cs
+++ DataWH/WebApplication/WebApplication/Controllers/ArtworkController.cs
@@ -39,7 +39,7 @@ namespace WebApplication.Controllers
         [HttpGet("getall")]
         public async Task<IActionResult> getallArtworks()
         {
-            StageDim.PopulateStageDimDate();
+           
             try
             {
                 ArtworkList artworkList = new ArtworkList();
