245c50b524c48a8e7076d6f6bb16e38a9eeff934
Valeriu Rusu <280167@via.dk>
2020-06-06 13:16:34 +0300

    Test Refactor

--- Android/app/src/androidTest/java/com/example/android_sep4/view/ManageAccountsTest.java
+++ Android/app/src/androidTest/java/com/example/android_sep4/view/ManageAccountsTest.java
@@ -37,6 +37,25 @@ public class ManageAccountsTest {
     @Rule
     public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
 
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+
     @Test
     public void manageAccountsTest() {
         ViewInteraction overflowMenuButton = onView(
@@ -71,24 +90,5 @@ public class ManageAccountsTest {
         appCompatImageButton.perform(click());
 
         pressBack();
-    }
-
-    private static Matcher<View> childAtPosition(
-            final Matcher<View> parentMatcher, final int position) {
-
-        return new TypeSafeMatcher<View>() {
-            @Override
-            public void describeTo(Description description) {
-                description.appendText("Child at position " + position + " in parent ");
-                parentMatcher.describeTo(description);
-            }
-
-            @Override
-            public boolean matchesSafely(View view) {
-                ViewParent parent = view.getParent();
-                return parent instanceof ViewGroup && parentMatcher.matches(parent)
-                        && view.equals(((ViewGroup) parent).getChildAt(position));
-            }
-        };
     }
 }
