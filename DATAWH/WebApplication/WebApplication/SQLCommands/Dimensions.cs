f932298bbfe8e7c5e5a7d21fe7054da21a0ff7e7
Florin Bordei <florin.brd@gmail.com>
2020-06-09 06:46:54 +0200

    dimtime

--- DATAWH/WebApplication/WebApplication/SQLCommands/Dimensions.cs
+++ DATAWH/WebApplication/WebApplication/SQLCommands/Dimensions.cs
@@ -114,6 +114,93 @@ namespace WebApplication.SQLCommands
              return null;
          }
          
+         public static async Task<IActionResult> CreateDimTableAndPopulate()
+         {
+             await using var sqlConnection = new SqlConnection(Dimensions.MuseumConnectionString);
+             await sqlConnection.OpenAsync();
+             var commandCreateLastUpdate = sqlConnection.CreateCommand();
+            
+             commandCreateLastUpdate.CommandText = 
+                 @"CREATE TABLE [DimTime]
+(
+    [TimeId] [int] IDENTITY(1,1) NOT NULL,
+    [Time] [time](0) NULL,
+    [Hour] [int] NULL,
+    [Minute] [int] NULL,
+    [AMPM] [varchar](2) NOT NULL,
+    [DayPartEN] [varchar](10) NULL,
+    [HourFromTo12] [varchar](17) NULL,
+    [HourFromTo24] [varchar](13) NULL,
+    [Notation12] [varchar](10) NULL,
+    [Notation24] [varchar](10) NULL
+);
+ 
+
+GO
+ 
+-- Create a time and a counter variable for the loop
+DECLARE @Time as time;
+SET @Time = '0:00';
+ 
+DECLARE @counter as int;
+SET @counter = 0;
+ 
+ 
+-- Two variables to store the day part for two languages
+DECLARE @daypartEN as varchar(20);
+set @daypartEN = '';
+
+ 
+ 
+-- Loop 1440 times (24hours * 60minutes)
+WHILE @counter < 1440
+BEGIN
+ 
+    -- Determine datepart
+    SELECT  @daypartEN = CASE
+                         WHEN (@Time >= '0:00' and @Time < '6:00') THEN 'Night'
+                         WHEN (@Time >= '6:00' and @Time < '12:00') THEN 'Morning'
+                         WHEN (@Time >= '12:00' and @Time < '18:00') THEN 'Afternoon'
+                         ELSE 'Evening'
+                         END
+    ,   
+ 
+    INSERT INTO DimTime ([Time]
+                       , [Hour]
+                       , [Minute]
+                       , [AMPM]
+                       , [DayPartEN]           
+                       , [HourFromTo12]
+                       , [HourFromTo24]
+                       , [Notation12]
+                       , [Notation24])
+                VALUES (@Time
+                       , DATEPART(Hour, @Time) + 1
+                       , DATEPART(Minute, @Time) + 1
+                       , DATEPART(Hour, @Time)
+                       , DATEPART(Minute, @Time)
+                       , CASE WHEN (DATEPART(Hour, @Time) < 12) THEN 'AM' ELSE 'PM' END
+                       , @daypartEN                
+                       , CONVERT(varchar(10), DATEADD(Minute, -DATEPART(Minute,@Time), @Time),100)  + ' - ' + CONVERT(varchar(10), DATEADD(Hour, 1, DATEADD(Minute, -DATEPART(Minute,@Time), @Time)),100)
+                       , CAST(DATEADD(Minute, -DATEPART(Minute,@Time), @Time) as varchar(5)) + ' - ' + CAST(DATEADD(Hour, 1, DATEADD(Minute, -DATEPART(Minute,@Time), @Time)) as varchar(5))
+                       , CONVERT(varchar(10), @Time,100)
+                       , CAST(@Time as varchar(5))
+                       );
+ 
+
+    SET @Time = DATEADD(minute, 1, @Time);
+ 
+
+    set @counter = @counter + 1;
+END";
+             
+             await commandCreateLastUpdate.ExecuteNonQueryAsync();
+             await commandCreateLastUpdate.DisposeAsync();
+             await sqlConnection.CloseAsync(); 
+
+             return null;
+         }
+         
       
     }
 }
